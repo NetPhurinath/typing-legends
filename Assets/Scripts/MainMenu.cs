0c743211ab830305d8b990fb29ee7cd436ac6369
Supawat Chaimoon <157001616+SupawatChaimoon@users.noreply.github.com>
2025-11-03 15:48:20 +0700

    apply gitignore correctly

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+   public void StartGame()
+    {
+        SceneManager.LoadSceneAsync("Level 1");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
